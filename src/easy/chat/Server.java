f03817d28c276e1abd7bf8271974c1941b3b391b
Mahfuz <sayhello.mahfuz@gmail.com>
2024-05-01 02:50:16 +0600

    add header icons

--- src/easy/chat/Server.java
+++ src/easy/chat/Server.java
@@ -5,8 +5,10 @@
 package easy.chat; // package
 import javax.swing.*; // for GUI
 import java.awt.*; // for set background and others color, styles
+import java.awt.event.*;
 
-public class Server extends JFrame{
+
+public class Server extends JFrame implements ActionListener{
     Server(){
         
         setLayout(null);
@@ -14,15 +16,75 @@ public class Server extends JFrame{
         JPanel p1 = new JPanel();
         p1.setBackground(new Color(7, 94, 84));
         p1.setBounds(0, 0, 450, 70);
+        p1.setLayout(null);
         add(p1);
+        
+        // back icon
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+        Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel back = new JLabel(i3);
+        back.setBounds(5, 20, 25, 25);
+        p1.add(back);
+        
+        back.addMouseListener(new MouseAdapter(){
+            public  void mouseClicked(MouseEvent ae){
+                System.exit(0);
+            }
+        });
+        
+        
+        // profile image
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40, 10, 50, 50);
+        p1.add(profile);
+        
+        
+        // video icon
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel video = new JLabel(i9);
+        video.setBounds(300, 20, 30, 30);
+        p1.add(video);
+        
+        
+        // phone icon
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        Image i11 = i10.getImage().getScaledInstance(35, 30, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+        JLabel phone = new JLabel(i12);
+        phone.setBounds(360, 20, 35, 30);
+        p1.add(phone);
+        
+        
+        // 3 dot icon
+        ImageIcon i13 = new ImageIcon(ClassLoader.getSystemResource("icons/3icon.png"));
+        Image i14 = i13.getImage().getScaledInstance(10, 25, Image.SCALE_DEFAULT);
+        ImageIcon i15 = new ImageIcon(i14);
+        JLabel more = new JLabel(i15);
+        more.setBounds(420, 20, 10, 25);
+        p1.add(more);
         
         setSize(450, 700);
         setLocation(700, 100);
         getContentPane().setBackground(Color.white);
         
         
+        
+        
         setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+    }
+    
+    
     public static void main(String[] args) {
         
         new Server();
